00b4453d8ce4dc0ba93ed0bcbffbbac313ff9a61
Tomvictor <vjtomvictor@icloud.com>
2016-07-17 20:54:08 +0530

    Search functionality added
    Only to the note list page

--- mysite/views.py
+++ mysite/views.py
@@ -1,31 +1,41 @@
-from django.shortcuts import render,get_object_or_404,HttpResponseRedirect,redirect
+from django.shortcuts import render, get_object_or_404, HttpResponseRedirect, redirect
 from django.contrib import messages
 from django.http import Http404
 from .forms import NoteForm
 from .models import notes
 from django.utils import timezone
+
+
 # Create your views here.
 
 def home(request):
-    if  request.user.is_authenticated():
+    if request.user.is_authenticated():
         all_notes = notes.objects.order_by('published_date')
+        query = request.GET.get("q")
+        if query:
+            all_notes = notes.objects.filter(title__icontains=query)
+
         return render(request, 'home.html', {'notes': all_notes})
     else:
         return redirect("mysite:login-page")
 
-    #return render(request,'home.html',{'notes':all_notes})
+        # return render(request,'home.html',{'notes':all_notes})
+
 
 def login(request):
-    return render(request,'login.html',{})
+    return render(request, 'login.html', {})
+
 
 def sign_up(request):
-    return render(request,'thanks-for-new-registration.html',{})
+    return render(request, 'thanks-for-new-registration.html', {})
 
-def note_detail(request,id = None):
+
+def note_detail(request, id=None):
     if not request.user.is_authenticated():
         raise Http404
-    current_note = get_object_or_404(notes,id=id)
-    return render(request,'single-note.html',{'this_note':current_note})
+    current_note = get_object_or_404(notes, id=id)
+    return render(request, 'single-note.html', {'this_note': current_note})
+
 
 def create_notes(request):
     if not request.user.is_authenticated():
@@ -41,21 +51,23 @@ def create_notes(request):
         return HttpResponseRedirect(instance.get_absolute_url())
     return render(request, 'note-form.html', {'form': form})
 
-def update_notes(request,id = None):
+
+def update_notes(request, id=None):
     if not request.user.is_authenticated():
         raise Http404
-    instance = get_object_or_404(notes,id=id)
-    form = NoteForm(request.POST or None, instance= instance)
+    instance = get_object_or_404(notes, id=id)
+    form = NoteForm(request.POST or None, instance=instance)
     if form.is_valid():
         instance = form.save(commit=False)
         print(form.cleaned_data.get("title"))
-        instance.last_update_date=timezone.now()
+        instance.last_update_date = timezone.now()
         instance.save()
         messages.success(request, "Succesfully Updated your note")
         return HttpResponseRedirect(instance.get_absolute_url())
     return render(request, 'note-form.html', {'form': form})
 
-def delete_notes(request,id = None):
+
+def delete_notes(request, id=None):
     if not request.user.is_authenticated():
         raise Http404
     instance = get_object_or_404(notes, id=id)
@@ -67,4 +79,4 @@ def delete_notes(request,id = None):
 def scrum(request):
     if not request.user.is_authenticated():
         raise Http404
-    return render(request,'scrum.html',{})
+    return render(request, 'scrum.html', {})
